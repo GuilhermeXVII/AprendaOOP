1434489896a87f5a89c46f9f88f4360b77c7d962
𝐺𝑢𝑖𝑙ℎ𝑒𝑟𝑚𝑒 <150200952+GuilhermeXVII@users.noreply.github.com>
2026-03-22 23:03:58 -0300

    Add files via upload

---
+++ Version0.0.1/Oficina.java
@@ -0,0 +1,17 @@
+//Version 0.0.1
+
+//Vamos iniciar o nosso código nessa versão
+//Aqui temos a nossca class Oficina, ela sera o nosso código fonte
+//Mas o que é código fonte?
+//Código fonte é aonde vai ficar todas as informações e comportamentos do nosso programa
+public class Oficina{
+
+    //Aqui temos os atributos da nossa oficina:
+    String nome;
+    String cpf;
+    String carro;
+    int anoCarro;
+    String modelo;
+
+    //Agora vamos criar uma classe chamada TestaCliente.java
+}
---
+++ Version0.0.1/TestaCliente.java
@@ -0,0 +1,21 @@
+//Dentro da classe TestaCliente.java
+public class TestaCliente{
+    public static void main(String[] args){
+        //Para instânciar um objeto da nossa classe Oficina, fazemos:
+        Oficina primeiroClienteGuilherme = new Oficina();
+
+        //Usamos o new para instanciar o "primeiroClienteGuilherme"
+
+        //Como eu posso atribuir valores aos atributos para a nossa primeira instancia apos
+        //cria-lá?
+        primeiroClienteGuilherme.nome="Guilherme";
+        System.out.println("Nosso primeiro cliente se chama:" + primeiroClienteGuilherme.nome);
+
+        //Ah cpf é string ou numero?
+        //Depende de quem fala e escreve o código, aqui vou deixa-lo como String para facilitar
+        primeiroClienteGuilherme.cpf="zzz.xxx.yyy-gg";
+        primeiroClienteGuilherme.carro="Chevrolet";
+        primeiroClienteGuilherme.anoCarro=1990;
+        primeiroClienteGuilherme.modelo="Ipanema 2.0 gls";
+    }
+}
